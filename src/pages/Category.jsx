4428da0839d9790265dc0978619ffdac309caae8
Jason <jasonutama0206@gmail.com>
2022-10-19 14:15:27 -0400

    Housing Market Placeb second Commit

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,4 +1,4 @@
-import { useEffec, useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import {
   collection,
--- src/pages/CreateListings.jsx
+++ src/pages/CreateListings.jsx
@@ -166,22 +166,6 @@ function CreateListings() {
     })
     console.log(imgUrls)
     setLoading(false)
-    //   const formDataCopy = {
-    //     ...formData,
-    //     imgUrls,
-    //     geolocation,
-    //     timestamp: serverTimestamp(),
-    //   }
-
-    //   formDataCopy.location = address
-    //   delete formDataCopy.images
-    //   delete formDataCopy.address
-    //   !formDataCopy.offer && delete formDataCopy.discountedPrice
-
-    //   const docRef = await addDoc(collection(db, 'listings'), formDataCopy)
-    //   setLoading(false)
-    //   toast.success('Listing saved')
-    //   navigate(`/category/${formDataCopy.type}/${docRef.id}`)
   }
 
   const onMutate = (e) => {
